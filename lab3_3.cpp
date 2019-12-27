d76e6772e13ecb06d8b1c70f3b9238100c6f73f3
pitchayachain <pitchaya_raihuay@cmu.ac.th>
2019-12-27 18:19:01 +0700

    version1

---
+++ lab3_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    float x = 2;
+    float y = 0;
+   while(x <= 69){
+   	
+   	y= y+(1/x);
+   	x=x+1;
+   	
+   }
+   cout<< y;
+   return 0;
+}
